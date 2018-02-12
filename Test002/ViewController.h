3675a67e386ac9fe1035c4a056519b57914ad50f
886wys <886wys@sina.com>
2018-02-12 17:38:45 +0800

    第二次修改

---
+++ Test002/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test002
+//
+//  Created by 王永顺 on 2018/2/12.
+//  Copyright © 2018年 EasonWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test002/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test002
+//
+//  Created by 王永顺 on 2018/2/12.
+//  Copyright © 2018年 EasonWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
